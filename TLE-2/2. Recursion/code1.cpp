b69b239eeee61331b6ee524191d8e91dc1d0816d
DINESH-1121 <cheekuridinesh1121@gmail.com>
2023-03-31 22:12:03 +0530

    code

--- TLE-2/2. Recursion/code1.cpp
+++ TLE-2/2. Recursion/code1.cpp
@@ -1,23 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mod 1000000007
-void solve()
-{
-       
-}
-int main() {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+vector<vector<int>> ans;
 
-    ll test=1;
-//cin>>test;
-    while(test--)
-    {
-        solve();
+int fun1(int i)
+{
+    if (i == 1)
+        return 1;
+    return fun1(i - 1) + i;
+}
+
+/**
+ * knapsack: find out maximum (no consecutive can be selected)
+ *
+ * RECURSION:
+ * if(i>=n) return;
+ * return max(w[i]+fun(i+2,n), fun(i+1,n))
+ *
+ * TC: O(2^N)
+ */
+
+void subsets(vector<int> v,int i,vector<int> v1){
+    if(i==v.size()){
+        ans.push_back(v1);
+        return;
     }
-    return 0;
+    subsets(v,i+1,v1);
+    v1.push_back(v[i]);
+    subsets(v,i+1,v1);
+}
+
+int main()
+{
+    // vector<int> v({1,2,3}),v1;
+    // subsets(v,0,v1);
+    // for(auto x:ans)
+    // {
+    //     for(auto y:x)
+    //         cout << y << " ";
+    //     cout << endl;
+    // }
+
+    
 }
--- TLE-2/sample.cpp
+++ TLE-2/sample.cpp
@@ -1,7 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int fun1(int i)
+{
+    if (i == 1)
+        return 1;
+    return fun1(i - 1) + i;
+}
+
+/**
+ * knapsack: find out maximum (no consecutive can be selected)
+ *
+ * RECURSION:
+ * if(i>=n) return;
+ * return max(w[i]+fun(i+2,n), fun(i+1,n))
+ *
+ * TC: O(2^N)
+ */
+
 
 int main()
 {
-    cout << "dinesh" << endl;
+
 }
